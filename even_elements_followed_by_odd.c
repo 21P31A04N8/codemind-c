6dfd68ef6167e8ebaaee083c0440406a6b316202
21P31A04N8 <99869346+21P31A04N8@users.noreply.github.com>
2022-11-09 15:59:50 +0530

    #include<stdio.h> int main() {     int n,i,a,b,arr[100],sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }
        }
        for(i=0;i<n;i++)
        {
            if(arr[i]%2==1)
            {
                printf("%d ",arr[i]);
            }
    
        }
    }
    /***
    n=int(input())
    arr=list(map(int,input().split()))
    z=[]
    for i in range(len(arr)):
        if i%2==1:
            z.append(i)
    for i in range(1,n):
        if i%2==0:
            z.append(i)
    print(z)
    ***/

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b,arr[100],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==1)
+        {
+            printf("%d ",arr[i]);
+        }
+
+    }
+}
+/***
+n=int(input())
+arr=list(map(int,input().split()))
+z=[]
+for i in range(len(arr)):
+    if i%2==1:
+        z.append(i)
+for i in range(1,n):
+    if i%2==0:
+        z.append(i)
+print(z)
+***/
